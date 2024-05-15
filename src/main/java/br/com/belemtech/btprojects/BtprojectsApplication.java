10ce87b5c13208b9b605a29d11c950e5dbf954f2
Ivanildo Ferreira <isivanildo@hotmail.com>
2024-05-15 19:37:41 -0300

    Iniciando o projeto - Primeiro commit

---
+++ src/main/java/br/com/belemtech/btprojects/BtprojectsApplication.java
@@ -0,0 +1,13 @@
+package br.com.belemtech.btprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtprojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtprojectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/belemtech/btprojects/BtprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.belemtech.btprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtprojectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
